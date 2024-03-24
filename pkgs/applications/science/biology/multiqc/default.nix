e1a55b6b775a312216df0077e24cae4976d4e5a0
Alexis Praga <alexis.praga@proton.me>
2024-03-24 23:34:02 +0100

    Trying to make pytest work

--- pkgs/applications/science/biology/multiqc/default.nix
+++ pkgs/applications/science/biology/multiqc/default.nix
@@ -1,14 +1,25 @@
-{ python3Packages, fetchPypi, lib }:
+{ lib
+, python3Packages
+, fetchPypi
+, fetchFromGitHub
+}:
+
+let  test-data = fetchFromGitHub {
+    owner = "MultiQC";
+    repo = "test-data";
+    rev = "d3e5dc356ef25011287f1ba65605d2ef82bb84e5";
+    hash = "";
+  }; in
 
 python3Packages.buildPythonApplication rec {
-  version = "1.15";
+  version = "1.21";
   pname = "multiqc";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-zlNZoSImz0zjcsb9rRQs/irnUB/6l6x6q1RM7U216jw=";
+    sha256 = "sha256-Y7yH4lHb94jcyKReWEguoJsm0FlXv1DHfGhNXwlypJU=";
   };
-  doCheck = false;
+  # doCheck = false;
   # Specify dependencies
   propagatedBuildInputs = with python3Packages; [
     click
@@ -28,6 +39,17 @@ python3Packages.buildPythonApplication rec {
     spectra
   ];
 
+# checkPhase = """
+    # MULTIQC_TEST_ROOT=${test-data} bash test/run_unit_tests.sh
+# """;
+  nativeCheckInputs = [
+    python3Packages.pytestCheckHook
+  ];
+
+ # requires additional data
+  pytestFlagsArray = [
+    "${test-data}"
+  ];
   meta = with lib; {
     description = "create a single report with interactive plots for multiple bioinformatics analyses across many samples.";
     homepage = "https://multiqc.info/";
