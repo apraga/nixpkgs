069feabb89475f41c953b38bdffec60b647dd986
Alexis Praga <alexis.praga@proton.me>
2024-07-13 23:50:03 +0200

    Forcing python 3.11 : compiles

--- pkgs/development/python-modules/clu/default.nix
+++ pkgs/development/python-modules/clu/default.nix
@@ -12,6 +12,7 @@
   typing-extensions,
   wrapt,
   buildPythonPackage,
+  pythonAtLeast,
   fetchPypi,
   setuptools,
   wheel,
@@ -20,6 +21,9 @@
 buildPythonPackage rec {
   pname = "clu";
   version = "0.0.12";
+
+  #--- Python 3.11 while waiting for tensorflow to support 3.12
+  disabled = pythonAtLeast "3.12";
 
   src = fetchPypi {
     inherit pname version;
